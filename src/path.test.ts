0cdeee7befdf074ec03b858ebf22e71f6b710938
madcampos <609904+madcampos@users.noreply.github.com>
2026-05-18 21:50:24 -0400

    feat: add path functions

---
+++ src/path.test.ts
@@ -0,0 +1,167 @@
+import { describe, expect, test } from 'vitest';
+import { basename, dirname, extname, normalize, pathToSegments, resolve } from './path';
+
+describe('pathToSegments', () => {
+	test('A single slash returns no segment', () => {
+		expect(pathToSegments('/')).toEqual([]);
+	});
+
+	test('An empty string returns no segments', () => {
+		expect(pathToSegments('')).toEqual([]);
+	});
+
+	test('A single lone percentage sign gets correctly handled', () => {
+		expect(pathToSegments('%')).toEqual(['%']);
+	});
+
+	test('URI encoded strings get correctly handled', () => {
+		expect(pathToSegments('foo%20bar/baz%2Fqux')).toEqual(['foo bar', 'baz', 'qux']);
+	});
+
+	test('Multiple slashes get collapsed', () => {
+		expect(pathToSegments('foo///bar//baz')).toEqual(['foo', 'bar', 'baz']);
+	});
+});
+
+describe('dirname', () => {
+	test('Path is empty', () => {
+		expect(dirname('')).toBe('');
+	});
+
+	test('Path is root', () => {
+		expect(dirname('/')).toBe('/');
+	});
+
+	test('Trailing slash is ignored', () => {
+		expect(dirname('/foo/bar/')).toBe('/foo');
+	});
+
+	test('Path ends in a file', () => {
+		expect(dirname('/foo/bar/baz.ext')).toBe('/foo/bar');
+	});
+
+	test('Path ends in a directory', () => {
+		expect(dirname('/foo/bar')).toBe('/foo');
+	});
+
+	test('Path is first level', () => {
+		expect(dirname('/foo')).toBe('/');
+	});
+
+	test('Path is relative', () => {
+		expect(dirname('foo/bar')).toBe('foo');
+		expect(dirname('foo')).toBe('');
+	});
+});
+
+describe('basename', () => {
+	test('Path is empty', () => {
+		expect(basename('')).toBe('');
+	});
+
+	test('Path is root', () => {
+		expect(basename('/')).toBe('');
+	});
+
+	test('Path ends in a file', () => {
+		expect(basename('/foo/bar/baz.txt')).toBe('baz.txt');
+	});
+
+	test('Path ends in a directory', () => {
+		expect(basename('/foo/bar/')).toBe('bar');
+		expect(basename('/foo/bar')).toBe('bar');
+	});
+
+	test('Path with suffix removes suffix', () => {
+		expect(basename('/foo/bar/baz.txt', '.txt')).toBe('baz');
+		expect(basename('index.html', '.html')).toBe('index');
+	});
+});
+
+describe('extname', () => {
+	test('Empty file name', () => {
+		expect(extname('')).toBe('');
+	});
+
+	test('File name with no extension', () => {
+		expect(extname('test')).toBe('');
+	});
+
+	test('File name with extension', () => {
+		expect(extname('test.ext')).toBe('.ext');
+	});
+
+	test('File name with composite extension', () => {
+		expect(extname('test.e.xt')).toBe('.xt');
+	});
+
+	test('File name with ending dot', () => {
+		expect(extname('test.')).toBe('.');
+	});
+
+	test('File name starting with dot and no extension', () => {
+		expect(extname('.test')).toBe('');
+	});
+
+	test('File name starting with dot and extension', () => {
+		expect(extname('.test.ext')).toBe('.ext');
+	});
+});
+describe('resolve', () => {
+	test('No path resolves to root', () => {
+		expect(resolve()).toBe('/');
+	});
+
+	test('Empty paths are skipped', () => {
+		expect(resolve('', 'foo', '', 'bar')).toBe('/foo/bar');
+	});
+
+	test('Relative path gets resolved from root', () => {
+		expect(resolve('foo/bar')).toBe('/foo/bar');
+	});
+
+	test('Relative path gets resolved from previous path', () => {
+		expect(resolve('/foo', 'bar', 'baz')).toBe('/foo/bar/baz');
+	});
+
+	test('Absolute path resets the resolving path', () => {
+		expect(resolve('/foo', '/bar', 'baz')).toBe('/bar/baz');
+	});
+
+	test('".." goes back one dir', () => {
+		expect(resolve('/foo/bar', '..', 'baz')).toBe('/foo/baz');
+	});
+
+	test('".." doesn\'t go back further than root', () => {
+		expect(resolve('..', '..', '..', '..', '..')).toBe('/');
+	});
+
+	test('"../" moves the result back', () => {
+		expect(resolve('foo', 'bar', '../baz')).toBe('/foo/baz');
+	});
+
+	test('"." is skipped', () => {
+		expect(resolve('/foo', '.', 'bar')).toBe('/foo/bar');
+	});
+
+	test('"./" gets handled as relative path', () => {
+		expect(resolve('/foo', './bar')).toBe('/foo/bar');
+	});
+});
+
+describe('normalize', () => {
+	test('Default replace resolves restricted characters', () => {
+		expect(normalize('foo/bar/baz?')).toBe('foo/bar/baz%3f');
+		expect(normalize('foo:bar')).toBe('foo%3abar');
+	});
+
+	test('Default replace resolves restricted names', () => {
+		expect(normalize('CON')).toBe('%43%4f%4e');
+		expect(normalize('COM1')).toBe('%43%4f%4d%31');
+	});
+
+	test('Custom replace works', () => {
+		const customReplacer = (segment: string) => segment.toUpperCase();
+		expect(normalize('foo/bar', customReplacer)).toBe('FOO/BAR');
+	});
+});
---
+++ src/path.ts
@@ -0,0 +1,190 @@
+/**
+ * Split a path string into segments and normalize it.
+ *
+ * The normalization does the following:
+ * - Removes empty path segments.
+ * - Decode from URI parts to handle paths coming from URLs.
+ *
+ * @param path The path to split into segments
+ */
+export function pathToSegments(path: string) {
+	const decodedPath = decodeURIComponent(path.replaceAll(/%(?![0-9a-fA-F]{2})/giu, '%25'));
+	const segments = decodedPath.split('/').filter(Boolean).map((part) => part.trim());
+
+	return segments;
+}
+
+/**
+ * Returns the directory name, given a path string.
+ * It is similar to node's `dirname` fucntion.
+ *
+ * @param path The path to get the directory name
+ */
+export function dirname(path: string) {
+	const segments = pathToSegments(path);
+	const pathBase = path.startsWith('/') ? '/' : '';
+
+	if (segments.length <= 1) {
+		return pathBase;
+	}
+
+	const dirSegments = segments.slice(0, -1);
+
+	return `${pathBase}${dirSegments.join('/')}`;
+}
+
+/**
+ * Returns the name for the last part of a given path string.
+ * It is similar to node's `basename` function.
+ *
+ * @param path The path to get the last part.
+ * @param suffix An optional extension to remove from the item, like an extension.
+ */
+export function basename(path: string, suffix?: string) {
+	const segments = pathToSegments(path);
+	const lastSegment = segments.at(-1) ?? '';
+
+	if (suffix && lastSegment.endsWith(suffix)) {
+		return lastSegment.slice(0, -suffix.length);
+	}
+
+	return lastSegment;
+}
+
+/**
+ * Returns the extension of the path, from the last occurrence of the `.` (dot) to the end of the string.
+ * Returns an empty string if there is no dot or if the only dot is in the start of the string.
+ * It is similar to node's `extname` function.
+ *
+ * @param path The path to get the extension
+ */
+export function extname(path: string) {
+	const base = basename(path);
+	const index = base.lastIndexOf('.');
+
+	if (index <= 0) {
+		return '';
+	}
+
+	return base.slice(index);
+}
+
+/**
+ * Resolves a sequence of paths or path segments into an absolute path.
+ * It is similar to node's `resolve` function.
+ *
+ * **Note**: If no absolute path is provided, the function prepends a `/`. It assumes the starting point is _always_ the file system root.
+ *
+ * @param paths A sequence of paths or path segments
+ */
+export function resolve(...paths: string[]) {
+	// INfO: start by concatenating all paths
+	let fullPath = '';
+	for (const path of paths) {
+		if (path.startsWith('/')) {
+			fullPath = path;
+		} else {
+			fullPath = fullPath ? `${fullPath}/${path}` : path;
+		}
+	}
+
+	// INFO: prepend initial dash
+	if (!fullPath.startsWith('/')) {
+		fullPath = `/${fullPath}`;
+	}
+
+	// INFO: resolve `..`, `.`, and empty segments
+	const stack: string[] = [];
+	const segments = pathToSegments(fullPath);
+
+	for (const segment of segments) {
+		if (segment === '..') {
+			stack.pop();
+		} else if (segment !== '.' && segment !== '') {
+			stack.push(segment);
+		}
+	}
+
+	return `/${stack.join('/')}`;
+}
+
+const RESTRICTED_NAMES = [
+	'CON',
+	'PRN',
+	'AUX',
+	'NUL',
+	'COM1',
+	'COM2',
+	'COM3',
+	'COM4',
+	'COM5',
+	'COM6',
+	'COM7',
+	'COM8',
+	'COM9',
+	'LPT1',
+	'LPT2',
+	'LPT3',
+	'LPT4',
+	'LPT5',
+	'LPT6',
+	'LPT7',
+	'LPT8',
+	'LPT9'
+];
+
+const RESTRICTED_CHARACTERS = [
+	// INFO: C0 control characters
+	// oxlint-disable-next-line no-magic-numbers
+	...new Array(32).fill('').map((_, i) => String.fromCharCode(i)),
+	// INFO: "del" character
+	'\x7f',
+	// INFO: C1 control characters
+	// oxlint-disable-next-line no-magic-numbers
+	...new Array(32).fill('').map((_, i) => String.fromCharCode(128 + i)),
+	// INFO: characters not allowed on Windows/Linux/Mac
+	...['*', '"', '/', '\\', '>', '<', ':', '|', '?'],
+	// INFO: also add "%" to make it easier to decode lone "%" symbols
+	'%'
+];
+
+function defaultReplacer(segment: string) {
+	if (segment === '.') {
+		return '%2e';
+	}
+
+	if (segment === '..') {
+		return '%2e%2e';
+	}
+
+	if (RESTRICTED_NAMES.includes(segment)) {
+		// oxlint-disable-next-line typescript/no-misused-spread
+		return [...segment].map((char) => `%${char.charCodeAt(0).toString(16)}`).join('');
+	}
+
+	// oxlint-disable-next-line typescript/no-misused-spread
+	const codePoints = [...segment];
+	let normalizedSegment = '';
+
+	for (const codePoint of codePoints) {
+		if (RESTRICTED_CHARACTERS.includes(codePoint)) {
+			normalizedSegment += `%${codePoint.charCodeAt(0).toString(16)}`;
+		} else {
+			normalizedSegment += codePoint;
+		}
+	}
+
+	return normalizedSegment.trim();
+}
+
+/**
+ * Normalizes a path string, taking care of restricted characters and names.
+ *
+ * @param path The path to normalize.
+ * @param replacer A custom replacer function that will be invoked for each segment of the path.
+ */
+export function normalize(path: string, replacer?: (segment: string) => string) {
+	const segments = pathToSegments(path);
+
+	return segments.map((segment) => (replacer ?? defaultReplacer)(segment)).join('/');
+}
